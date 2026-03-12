420b3a84a4ea66652c6560b4bf5d8d79b6d7b831
ahmad <s12112906@stu.najah.edu>
2026-03-12 18:31:00 +0300

    New Arival component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 import Hero from "../../components/Hero/Hero.jsx";
 import Catagories from '../../components/Catagories/Catagories.jsx';
 import Products from '../../components/Products/Products.jsx';
-
+import NewArrival from '../../components/NewArrival/NewArrival.jsx';
 export default function Home() {
   return (
     <>
       <Hero />
 <Catagories showMore={true} />
       <Products />
+      <NewArrival />
     </>
   );
 }
