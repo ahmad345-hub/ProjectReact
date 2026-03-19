d3bd89071c1a592aee562b72a1cb1d44191ef9d8
ahmad <s12112906@stu.najah.edu>
2026-03-19 17:47:15 +0300

    update add to cart hook

--- src/hooks/useAddtoCart.jsx
+++ src/hooks/useAddtoCart.jsx
@@ -1,20 +1,41 @@
-import React from 'react';
-import { useMutation, useQuery } from '@tanstack/react-query';
-import Authaxiosinstance from '../api/Authaxiosinstance';
-import { useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import Authaxiosinstance from "../api/Authaxiosinstance";
+
 export default function useAddtoCart() {
-const QueryClien=useQueryClient();
+  const queryClient = useQueryClient();
+
   const mutation = useMutation({
-mutationFn:async({ ProductId, Count })=>{
-    return await Authaxiosinstance.post("/carts/",{
-      ProductId: ProductId,
-       Count: Count,
-    })
-}, onSuccess:()=>{
-    QueryClien.invalidateQueries({ queryKey: ["Carts"] });
-    
-}
-})
+    mutationFn: async ({ ProductId, Count }) => {
+      return await Authaxiosinstance.post("/carts/", { ProductId, Count });
+    },
+    onSuccess: (data, variables) => {
+      queryClient.setQueryData(["Carts"], (oldData) => {
+        if (!oldData)
+          return {
+            items: [{ ProductId: variables.ProductId, count: variables.Count }],
+          };
+
+        const existingItemIndex = oldData.items.findIndex(
+          (item) => item.ProductId === variables.ProductId,
+        );
+        if (existingItemIndex !== -1) {
+          const newItems = [...oldData.items];
+          newItems[existingItemIndex].count += variables.Count;
+          return { ...oldData, items: newItems };
+        }
+
+        return {
+          ...oldData,
+          items: [
+            ...oldData.items,
+            { ProductId: variables.ProductId, count: variables.Count },
+          ],
+        };
+      });
+
+      queryClient.invalidateQueries({ queryKey: ["Carts"] });
+    },
+  });
 
   return mutation;
 }
